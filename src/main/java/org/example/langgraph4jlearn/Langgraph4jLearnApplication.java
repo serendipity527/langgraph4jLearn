aa6d7fb2f57c51bb6f437573f69ecf53dcb66e00
v-lizhengfan <serendipity_yihu@163.com>
2025-11-24 10:20:52 +0800

    Initial commit: Add langgraph4jLearn project

---
+++ src/main/java/org/example/langgraph4jlearn/Langgraph4jLearnApplication.java
@@ -0,0 +1,13 @@
+package org.example.langgraph4jlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langgraph4jLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Langgraph4jLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/langgraph4jlearn/Langgraph4jLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.langgraph4jlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langgraph4jLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
