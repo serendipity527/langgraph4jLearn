0830c24306add83085e55f9efe4114b014e092a0
v-lizhengfan <serendipity_yihu@163.com>
2025-11-25 17:37:07 +0800

    - Dynamic graph

---
+++ DynamicGraph/src/main/java/org/example/dynamicgraph/DynamicGraphApplication.java
@@ -0,0 +1,13 @@
+package org.example.dynamicgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicGraphApplication.class, args);
+    }
+
+}
---
+++ DynamicGraph/src/test/java/org/example/dynamicgraph/DynamicGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dynamicgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
